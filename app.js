acf1a7fe5ac03247c370c1e78f264fd6721a3f5f
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-12 12:23:40 +0800

    convert es6 imports to commonjs, update route names, connect do MongoDB Atlas

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import logger from 'morgan';
-import todoRoutes from './routes/todos.js';
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const todoRoutes = require('./routes/todos');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to MongoDB Atlas')
+);
 
 // middlwares
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(todoRoutes);
+app.use('/api/v1/todos', todoRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello there!');
