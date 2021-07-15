f9c3d004c9a7e4d70fe7626ba5b0bf4451bbf8b2
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 16:48:35 +0800

    update authentication function name

--- routes/todos.js
+++ routes/todos.js
@@ -1,28 +1,28 @@
 const router = require('express').Router();
-const verify = require('../helpers/verifyToken');
+const requireAuth = require('../middlewares/auth');
 
 // Get all todos
-router.get('/', verify, (req, res) => {
+router.get('/', requireAuth, (req, res) => {
   res.send('List of all items');
 });
 
 // Get a single todo item
-router.get('/:id', verify, (req, res) => {
+router.get('/:id', requireAuth, (req, res) => {
   res.send('Single Todo item');
 });
 
 // Create a new todo item
-router.post('/', verify, (req, res) => {
+router.post('/', requireAuth, (req, res) => {
   res.send('Adding a new todo');
 });
 
 // Update an existing todo item
-router.put('/:id', verify, (req, res) => {
+router.put('/:id', requireAuth, (req, res) => {
   res.send('Updating a todo');
 });
 
 // Delete a todo item
-router.delete('/:id', verify, (req, res) => {
+router.delete('/:id', requireAuth, (req, res) => {
   res.send('Item deleted');
 });
 
