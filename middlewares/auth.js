34580ed397bfc2a243fe3a5ff21e5277decae6bc
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-15 16:48:08 +0800

    add jwt auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+  const token = req.header('auth-token');
+
+  if (!token) return res.status(401).send('Access denied');
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid token', error);
+  }
+};
+
+module.exports = requireAuth;
