77f2272624bdad038b39d454d92ab668ec2f527a
Ahmad Ihsan <ihsanashi@gmail.com>
2021-07-27 17:53:00 +0800

    add permissions for each route method

--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwtAuthz = require('express-jwt-authz');
 const {
   getAllTodos,
   getSingleTodo,
@@ -8,18 +9,18 @@ const {
 } = require('../controllers/todos');
 
 // Get all todos
-router.get('/', getAllTodos);
+router.get('/', jwtAuthz(['read:todos']), getAllTodos);
 
 // Get a single todo item
-router.get('/:id', getSingleTodo);
+router.get('/:id', jwtAuthz(['read:todo']), getSingleTodo);
 
 // Create a new todo item
-router.post('/', postNewTodo);
+router.post('/', jwtAuthz(['create:todos']), postNewTodo);
 
 // Update an existing todo item
-router.put('/:id', updateTodo);
+router.put('/:id', jwtAuthz(['update:todo']), updateTodo);
 
 // Delete a todo item
-router.delete('/:id', deleteTodo);
+router.delete('/:id', jwtAuthz(['delete:todo']), deleteTodo);
 
 module.exports = router;
