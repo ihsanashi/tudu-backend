ae739804b3d8009f593dfcb385e14033abb15059
Ahmad Ihsan <ihsanashi@gmail.com>
2021-08-13 18:29:05 +0800

    add option to mongoose connect

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() =>
     app.listen(PORT, () => {
