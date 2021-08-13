481d4c1780270787727eb61bb7d497a936fbdfe4
Ahmad Ihsan <ihsanashi@gmail.com>
2021-08-13 17:17:23 +0800

    uadd controllers for all CRUD functions except update

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,28 +1,73 @@
 const Todo = require('../models/todo');
 
 const getAllTodos = async (req, res) => {
+  const { userId } = req.body;
   try {
-    const todos = await Todo.find();
-    res.status(200).json(todos);
+    if (!userId)
+      return res.status(400).json({
+        message: `Bad request. 'userId' field is required to be a part of request body to query the database.`,
+      });
+    const todos = await Todo.find({ userId });
+    return res.status(200).json(todos);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    return res.status(404).json({ message: error.message });
   }
 };
 
 const getSingleTodo = async (req, res) => {
-  res.send('Single todo item');
+  try {
+    let todoItem = await Todo.findById(req.params.id);
+    if (todoItem == null) {
+      return res.status(404).json({ message: 'Todo item not found' });
+    }
+
+    return res.status(200).json(todoItem);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-const postNewTodo = (req, res) => {
-  res.send('Creating a new todo');
+const postNewTodo = async (req, res) => {
+  const { title, description, userId } = req.body;
+
+  const todoItem = new Todo({
+    title,
+    description,
+    userId,
+  });
+
+  try {
+    if (!title) {
+      return res
+        .status(400)
+        .json({ message: 'Bad request. Title field is required.' });
+    }
+
+    const newTodo = await todoItem.save();
+    return res.status(201).json(newTodo);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
-const updateTodo = (req, res) => {
+// TODO! Work on this update todo function
+const updateTodo = async (req, res) => {
   res.send('Update a todo');
 };
 
-const deleteTodo = (req, res) => {
-  res.send('Item deleted');
+const deleteTodo = async (req, res) => {
+  try {
+    let todoItem = await Todo.findByIdAndDelete(req.params.id);
+    if (todoItem == null) {
+      return res.status(404).json({ message: 'Todo item not found' });
+    }
+
+    return res.status(200).json({
+      message: `Todo item titled '${todoItem.title}' successfully deleted.`,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
